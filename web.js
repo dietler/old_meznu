026171686c134bba6f20ea250bb0cd8868a01a76
Andy Dietler <andy@dietler.net>
2012-04-20 22:05:11 +0000

    more setup. in version 0.0.2 now.

--- web.js
+++ web.js
@@ -1,35 +1,40 @@
-/**
- * Module dependencies.
- */
- 
+// *******************************************************
+// expressjs template
+//
+// assumes: npm install express
+// defaults to jade engine, install others as needed
+//
+// assumes these subfolders:
+//   public/
+//   public/javascripts/
+//   public/stylesheets/
+//   views/
+//
 var express = require('express');
- 
 var app = module.exports = express.createServer();
- 
+var viewEngine = 'jade'; // modify for your view engine
 // Configuration
- 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', viewEngine);
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
- 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
- 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
+// *******************************************************
  
 // Routes
  
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Node Test'
+    title: 'Meznu'
   });
 });
  
