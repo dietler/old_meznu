6893d53343173ffafa0906886ba50513a4e47301
Andy Dietler <andy@dietler.net>
2012-04-26 04:03:14 +0000

    twitter oauth, take 1

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ What's on the menu?
 - [node.js](http://nodejs.org/)
 - [express](http://expressjs.com/)
 - [jade](http://jade-lang.com/)
+- [node-oauth](https://github.com/ciaranj/node-oauth)
 - [less](http://lesscss.org/)
 - [markdown](http://daringfireball.net/projects/markdown/)
--- web.js
+++ web.js
@@ -1,3 +1,15 @@
+var OAuth= require('oauth').OAuth;
+
+var oa = new OAuth(
+    "https://api.twitter.com/oauth/request_token",
+	"https://api.twitter.com/oauth/access_token",
+	"tFN7vdcZsTPbqMbNfFp8Ow",
+	"8uudRIdFbWlw3dZ4w8n6yv4BbjasFYtryc4A1smSjo",
+	"1.0",
+	"http://meznu.com/auth/twitter/callback",
+	"HMAC-SHA1"
+);
+
 // *******************************************************
 // expressjs template
 //
@@ -36,6 +48,45 @@ app.get('/', function(req, res){
   res.render('index', {
     title: 'Meznu'
   });
+});
+
+app.get('/auth/twitter', function(req, res){
+    oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+		if (error) {
+			console.log(error);
+			res.send("yeah no. didn't work.")
+		}
+		else {
+			req.session.oauth = {};
+			req.session.oauth.token = oauth_token;
+			console.log('oauth.token: ' + req.session.oauth.token);
+			req.session.oauth.token_secret = oauth_token_secret;
+			console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
+			res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
+	}
+	});
+});
+
+app.get('/auth/twitter/callback', function(req, res, next){
+    if (req.session.oauth) {
+		req.session.oauth.verifier = req.query.oauth_verifier;
+		var oauth = req.session.oauth;
+
+		oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
+		function(error, oauth_access_token, oauth_access_token_secret, results){
+			if (error){
+				console.log(error);
+				res.send("yeah something broke.");
+			} else {
+				req.session.oauth.access_token = oauth_access_token;
+				req.session.oauth,access_token_secret = oauth_access_token_secret;
+				console.log(results);
+				res.send("worked. nice one.");
+			}
+		}
+		);
+	} else
+		next(new Error("you're not supposed to be here."))
 });
  
 var port = process.env.PORT || 3000;
